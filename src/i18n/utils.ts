d4791ce217bf2ff92a920da492992877a7cee899
GaspardCulis <gaspard@gasdev.fr>
2024-06-30 10:36:12 +0200

    feat(i18n -> utils): Added `translateUrl` function

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -12,3 +12,11 @@ export function useTranslations(lang: keyof typeof ui) {
     return ui[lang][key] || ui[defaultLocale][key];
   };
 }
+
+export function translateUrl(url: URL, slug: string): string {
+  const lang = getLangFromUrl(url);
+  const currentUrl = new URL(url);
+  currentUrl.pathname = lang == defaultLocale ? `${slug}` : `/${lang}/${slug}`;
+  currentUrl.pathname += currentUrl.pathname.endsWith("/") ? "" : "/";
+  return currentUrl.toString();
+}
