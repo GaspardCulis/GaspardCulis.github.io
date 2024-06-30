c5d0bbe99197229a79c62821ca8fbf31b7cec7ad
GaspardCulis <gaspard@gasdev.fr>
2024-06-30 09:35:46 +0200

    feat(i18n): Create basic translation strings in "ui.ts"

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const defaultLocale = "en";
+export const locales = {
+  en: "en-US",
+  fr: "fr-FR",
+};
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.projects": "Projets",
+    "nav.resume": "My CV",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.about": "À propos",
+    "nav.resume": "Mon CV",
+  },
+} as const;
