47f00d519296bc92ab37911961fab238f4511d7e
GaspardCulis <gaspard@gasdev.fr>
2024-06-30 09:54:44 +0200

    feat(i18n + Hero): Hero component now uses i18n

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -9,10 +9,21 @@ export const ui = {
     "nav.home": "Home",
     "nav.projects": "Projets",
     "nav.resume": "My CV",
+
+    "hero.title": 'Hi, I\'m <span class="text-secondary">Gaspard Culis</span>.',
+    "hero.description":
+      "Passionate and versatile developer with a background in full-stack WebDev, GameDev, Machine Learning and more!",
+    "hero.contact": "Contact me",
   },
   fr: {
     "nav.home": "Accueil",
     "nav.about": "À propos",
     "nav.resume": "Mon CV",
+
+    "hero.title":
+      '<span class="text-secondary">Gaspard Culis</span> - Développeur full-stack',
+    "hero.description":
+      "Développeur passionné et polyvalent, fort d'une expérience en développement web full-stack, GameDev, Machine Learning et bien plus encore !",
+    "hero.contact": "Contactez moi",
   },
 } as const;
