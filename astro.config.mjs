bcdfd10a88ff8aa1d34d5d8929b03cecc5bec3ea
GaspardCulis <gaspard@gasdev.fr>
2024-06-30 09:31:43 +0200

    feat(astro.config): Configured i18n plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,30 @@
 import { defineConfig } from "astro/config";
 import { remarkReadingTime } from "./plugins/remark-reading-time.mjs";
 
+import { i18n, filterSitemapByDefaultLocale } from "astro-i18n-aut/integration";
 import tailwind from "@astrojs/tailwind";
+
+const defaultLocale = "en";
+const locales = {
+  en: "en-US", // the `defaultLocale` value must present in `locales` keys
+  es: "es-ES",
+  fr: "fr-CA",
+};
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.gasdev.fr",
-  integrations: [tailwind()],
+  trailingSlash: "always",
+  build: {
+    format: "directory",
+  },
+  integrations: [
+    tailwind(),
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+  ],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
